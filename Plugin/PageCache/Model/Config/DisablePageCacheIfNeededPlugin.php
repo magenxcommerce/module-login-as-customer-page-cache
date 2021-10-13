aec236f86dba940a259e308dc0ef5da6fd7cdd57
Yegor Shytikov <egorshitiov@gmail.com>
2021-10-13 13:22:28 -0700

    Magento Fork initial commit

--- Plugin/PageCache/Model/Config/DisablePageCacheIfNeededPlugin.php
+++ Plugin/PageCache/Model/Config/DisablePageCacheIfNeededPlugin.php
@@ -54,7 +54,7 @@ class DisablePageCacheIfNeededPlugin
     public function afterIsEnabled(Config $subject, $isEnabled): bool
     {
         if ($isEnabled) {
-            $disable = $this->scopeConfig->isSetFlag(
+            $disable = $this->scopeConfig->getValue(
                 'login_as_customer/general/disable_page_cache',
                 ScopeInterface::SCOPE_STORE
             );
